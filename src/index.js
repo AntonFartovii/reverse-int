61de73d88ea06fe4ca3db4840c2c446a71beb81e
AntonFartovii <anton@rza.by>
2022-07-20 23:27:29 +0300

    feat: create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if ( n < 0 ) { n = n * -1 }
+  const num = n.toString().split('').reverse().join('')
+    return Number(num)
 }
+
+// console.log ( reverse( -345) )
